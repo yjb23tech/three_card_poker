5bf0fcb6cdd016b3de6a87f4d90409d7d95740c2
yjb23tech <yjb23tech@gmail.com>
2023-12-30 01:43:39 +0000

    Mwahaha

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,15 @@
 const Home = () => {
     return (
         <div>
-            <h1>Casa Blanca</h1>
+            <h1>Earth and Mars</h1>
+            <h2>Earth and Mars are two planets within our own solar system</h2>
+
+            <h3>Which planet would you like to know better?</h3>
+
+            <ol>
+                <li>I would like to know more about Earth</li>
+                <li>I would like to know more about Mars</li>
+            </ol>
         </div>
     )
 }
