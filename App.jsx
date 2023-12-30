3d0d5fe15c4388395593a7e7e76303c44bbf1a88
yjb23tech <yjb23tech@gmail.com>
2023-12-30 01:20:58 +0000

    LaLaLa

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client"
+import Home from "./pages/Home.jsx"
+import Earth from "./pages/Earth.jsx"
+import Mars from "./pages/Mars.jsx"
 
 const App = () => {
     return (
         <div>
-            <h1>Hello New World</h1>
+            <Mars />
         </div>
     )
 }
