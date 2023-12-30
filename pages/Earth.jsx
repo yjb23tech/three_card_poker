7994bbd90618371bb9056da199acd4011b894e9b
yjb23tech <yjb23tech@gmail.com>
2023-12-30 01:18:32 +0000

    Crack

---
+++ pages/Earth.jsx
@@ -0,0 +1,9 @@
+const Earth = () => {
+    return (
+        <div>
+            <h1>Terra Firma</h1>
+        </div>
+    )
+}
+
+export default Earth; 
---
+++ pages/Mars.jsx
@@ -0,0 +1,9 @@
+const Mars = () => {
+    return (
+        <div>
+            <h1>The Red Planet</h1>
+        </div>
+    )
+}
+
+export default Mars; 
